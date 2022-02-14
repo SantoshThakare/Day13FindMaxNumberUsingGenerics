1f8e6b7571f62104b4904b87633cc104c53d9e94
SantoshThakare <santoshthakare50@gmail.com>
2022-02-14 17:56:27 +0530

    [santosh]: More than 3 parameter Use Option and Sorting

--- FindMaximumProblem/FindMax.cs
+++ FindMaximumProblem/FindMax.cs
@@ -8,23 +8,43 @@ namespace FindMaximumProblem
 {
     public class FindMax<T> where T : IComparable
     {
-        public T ReturnMax (T v1, T v2, T v3) 
+        public T ReturnMax(T v1, T v2, T v3, T v4)
         {
-            if (v1.CompareTo(v2) > 0 &&  v1 .CompareTo(v3) > 0)
+            if (v1.CompareTo(v2) > 0 && v1.CompareTo(v3) > 0 && v1.CompareTo(v4) > 0)
             {
                 return v1;
 
             }
-            if (v2.CompareTo (v3) > 0  && v2 .CompareTo(v1) >0)
+            if (v2.CompareTo(v3) > 0 && v2.CompareTo(v1) > 0 && v2.CompareTo(v4) > 0)
             {
                 return v2;
             }
-            else
+            if (v3.CompareTo(v1) > 0 && v3.CompareTo(v2) > 0 && v3.CompareTo(v4) > 0)
             {
                 return v3;
+            }
+            else
+            {
+                return v4;
             }
 
 
         }
+        public T[] value;
+        public FindMax(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+        public T MaxValue(params T[] value)
+        {
+            var Sorted_Values = Sort(value);
+            return Sorted_Values[1];
+        }
+       
     }
 }
--- Test1MaxNumber/UnitTest1.cs
+++ Test1MaxNumber/UnitTest1.cs
@@ -5,75 +5,100 @@ namespace Test1MaxNumber
 {
     public class Tests
     {
-        FindMax<int> Maxint = new FindMax<int>();
-        FindMax<double> Maxdouble = new FindMax<double>();
-        FindMax<string> Maxstring = new FindMax<string>();
+        int[] intArray =  { 1, 2, 3 };
+        double[] doubleArray = { 1d, 2d, 3d };
+        string[] stringArray = { "HCl", "Tcs", "Infosys" };
+
+        
+        
 
 
         [Test]
         public void FirstPositionMaxInt1()
         {
+            FindMax<int> Maxint = new FindMax<int>(intArray);
 
-
-            int max = Maxint.ReturnMax(5, 4, 3);
+            int max = Maxint.ReturnMax(5, 4, 3, 2);
             Assert.AreEqual(5, max);
         }
 
         [Test]
         public void SecondPositionMaxInt2()
         {
+            FindMax<int> Maxint = new FindMax<int>(intArray);
 
-            int max = Maxint.ReturnMax(4, 5, 3);
+            int max = Maxint.ReturnMax(4, 5, 3, 2);
             Assert.AreEqual(5, max);
         }
         [Test]
         public void ThirdPositionMaxInt3()
         {
-
-            int max = Maxint.ReturnMax(3, 4, 5);
+            FindMax<int> Maxint = new FindMax<int>(intArray);
+            int max = Maxint.ReturnMax(3, 4, 5, 2);
             Assert.AreEqual(5, max);
         }
         [Test]
+        public void FourthPositionMaxInt4()
+        {
+            FindMax<int> Maxint = new FindMax<int>(intArray);
+            int max = Maxint.ReturnMax(3, 4, 2, 5);
+            Assert.AreEqual(5, max);
+        }
+            [Test]
         public void FirstPositionMaxDouble1()
         {
-
-            double max = Maxdouble.ReturnMax(5.5d, 4.4d, 3.2d);
+            FindMax<double> Maxdouble = new FindMax<double>(doubleArray);
+            double max = Maxdouble.ReturnMax(5.5d, 4.4d, 3.3d, 2.2d);
             Assert.AreEqual(5.5d, max);
         }
 
         [Test]
         public void SecondPositionMaxDouble2()
         {
-
-            double max = Maxdouble.ReturnMax(4.4d, 5.5d, 3.2d);
+            FindMax<double> Maxdouble = new FindMax<double>(doubleArray);
+            double max = Maxdouble.ReturnMax(4.4d, 5.5d, 3.3d,2.2d );
             Assert.AreEqual(5.5d, max);
         }
         [Test]
         public void ThirdPositionMaxDouble3()
         {
-
-            double max = Maxdouble.ReturnMax(3.2d, 4.4d, 5.5d);
+            FindMax<double> Maxdouble = new FindMax<double>(doubleArray);
+            double max = Maxdouble.ReturnMax(3.2d, 4.4d, 5.5d, 2.2d);
+            Assert.AreEqual(5.5, max);
+        }
+        [Test]
+        public void FourthPositionMaxDouble4()
+        {
+            FindMax<double> Maxdouble = new FindMax<double>(doubleArray);
+            double max = Maxdouble.ReturnMax(3.2d, 4.4d, 2.2d, 5.5d);
             Assert.AreEqual(5.5, max);
         }
         [Test]
         public void FirstPositionMaxString1()
         {
-
-            string max = Maxstring.ReturnMax("TechMahindra", "Infosys", "HCL");
+            FindMax<string> Maxstring = new FindMax<string>(stringArray);
+            string max = Maxstring.ReturnMax("TechMahindra", "Infosys", "HCL" , "TCS");
             Assert.AreEqual("TechMahindra", max);
         }
 
         [Test]
         public void SecondPositionMaxString2()
         {
-
-            string max = Maxstring.ReturnMax("Infosys", "TechMahindra", "HCL");
+            FindMax<string> Maxstring = new FindMax<string>(stringArray);
+            string max = Maxstring.ReturnMax("Infosys", "TechMahindra", "HCL", "TCS");
             Assert.AreEqual("TechMahindra", max);
         }
         [Test]
         public void ThirdPositionMaxStr3()
         {
-            string max = Maxstring.ReturnMax("HCL", "Infosys", "TechMahindra");
+            FindMax<string> Maxstring = new FindMax<string>(stringArray);
+            string max = Maxstring.ReturnMax("HCL", "Infosys", "TechMahindra","TCS");
+            Assert.AreEqual("TechMahindra", max);
+        }
+        public void FourthPositionMaxStr4()
+        {
+            FindMax<string> Maxstring = new FindMax<string>(stringArray);
+            string max = Maxstring.ReturnMax("HCL", "Infosys", "TCS", "TechMahindraCS");
             Assert.AreEqual("TechMahindra", max);
         }
     }
